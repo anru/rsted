841f58fc4d37c8410477b865016fc3ceb3ae3c7b
Sergey Rublev <narma.nsk@gmail.com>
2011-09-09 22:33:08 +0400

    tab2spaces and more strict css find of textarea

--- static/scripts/editor.js
+++ static/scripts/editor.js
@@ -13,39 +13,39 @@ function getSelectedTheme() {
 
 
 function setPreviewHtml(html) {
-	var iframe = $('#browse')[0];
-	var doc = iframe.document;
-	if (iframe.contentDocument) {
-		doc = iframe.contentDocument; // For NS6
-	} else if (iframe.contentWindow) {
-		doc = iframe.contentWindow.document; // For IE5.5 and IE6
-	}
-	doc.open();
-	doc.writeln(html);
-	doc.close();
+    var iframe = $('#browse')[0];
+    var doc = iframe.document;
+    if (iframe.contentDocument) {
+        doc = iframe.contentDocument; // For NS6
+    } else if (iframe.contentWindow) {
+        doc = iframe.contentWindow.document; // For IE5.5 and IE6
+    }
+    doc.open();
+    doc.writeln(html);
+    doc.close();
 }
 
 var activeXhr = null;
 var lastContent = null;
 
 function genPreview() {
-    var self = $('#editor');
-	var rstContent = self.val();
+    var self = $('textarea#editor');
+    var rstContent = self.val();
     if (activeXhr || lastContent == rstContent) {
         //activeXhr.abort();
-		return;
+        return;
     }
-	lastContent = rstContent;
+    lastContent = rstContent;
     activeXhr = $.ajax({
         'url': '/srv/rst2html/',
         'data': {'rst': rstContent, 'theme': getSelectedTheme()},
-		'type': 'POST',
-		'error': function(xhr) {
-			setPreviewHtml(xhr.responseText);
-		},
+        'type': 'POST',
+        'error': function(xhr) {
+            setPreviewHtml(xhr.responseText);
+        },
         'success': function(response) {
             setPreviewHtml(response);
-			activeXhr = null;
+            activeXhr = null;
         }
     });
 }
@@ -53,15 +53,15 @@ function genPreview() {
 var timerId = null;
 
 function getCurrentLink(res) {
-	if (!res) {
-		return 'http://' + window.location.host + '/?theme=' + getSelectedTheme();
-	}
-	return 'http://' + window.location.host + '/?n=' + res + '&theme=' + getSelectedTheme();
+    if (!res) {
+        return 'http://' + window.location.host + '/?theme=' + getSelectedTheme();
+    }
+    return 'http://' + window.location.host + '/?n=' + res + '&theme=' + getSelectedTheme();
 }
 
 function showLinkDialog(url) {
     var showLinker = $('.showLinkDialog');
-            
+
     var self = $('#show_link');
     var offs = self.offset();
     showLinker.find('input').val(url);
@@ -73,25 +73,26 @@ function showLinkDialog(url) {
 
 
 $(function() {
-	//$('<button>Conver!</button>').click(genPreview).appendTo($('body'));
-	$('#editor').bind('change', genPreview).markItUp(mySettings);
-	timerId = window.setInterval(genPreview, 900);
-	window.setTimeout(function() {
-		$('#editor-td > div').css({'width': '100%', 'height': '96%'});
-	}, 200);
-	
-	$('.themes input').bind('change', function() {
-		lastContent = null;
-		genPreview()
-	});
-	
-	$('.showLinkDialog .hider').click(function() {
-		$('.showLinkDialog').hide('slow');
-	});
-	
-	
-	$('#show_link').click(function(e) {
-		
+    //$('<button>Conver!</button>').click(genPreview).appendTo($('body'));
+
+    $('textarea#editor').bind('change', genPreview).markItUp(mySettings);
+    timerId = window.setInterval(genPreview, 900);
+    window.setTimeout(function() {
+        $('#editor-td > div').css({'width': '100%', 'height': '96%'});
+    }, 200);
+
+    $('.themes input').bind('change', function() {
+        lastContent = null;
+        genPreview()
+    });
+
+    $('.showLinkDialog .hider').click(function() {
+        $('.showLinkDialog').hide('slow');
+    });
+
+
+    $('#show_link').click(function(e) {
+
         $.ajax({
             'url': '/srv/save_rst/',
             'type': 'POST',
@@ -100,11 +101,11 @@ $(function() {
                 showLinkDialog(getCurrentLink(response + ''));
             }
 
-		});
-		
-		e.preventDefault();
-		return false;
-	});
+        });
+
+        e.preventDefault();
+        return false;
+    });
 
     $('#as_pdf').click(function(e) {
         var form = $('#save_as_pdf');
