b57a53123dcec8962331a45c3ae54a168edb943b
Sergey Rublev <narma.nsk@gmail.com>
2011-09-09 10:49:30 +0400

    make redis_db configurable

--- application.py
+++ application.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.config.from_pyfile(os.environ.get('RSTED_CONF', 'settings.py'))
 
 def connect_redis():
-    return redis.Redis(host=app.config.get('REDIS_HOST', 'localhost'), port=app.config.get('REDIS_PORT', 6379), db=0)
+    return redis.Redis(host=app.config.get('REDIS_HOST', 'localhost'), port=app.config.get('REDIS_PORT', 6379), db=app.config.get('REDIS_DB', 0))
 
 @app.context_processor
 def ctx_pro():
